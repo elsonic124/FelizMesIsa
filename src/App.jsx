dda5bd84ca73b034780ec455497a1db5f08b34f1
Cristian Leal <projects@sinergicotechnology.com>
2024-07-25 18:11:27 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Feliz mes preciosa linda fantastica sublime titán bestia mastodonte leyenda frikitona epica</h1>
+        <p>Por circunstancias en la producción, he tenido que hacer esta web tan</p>
+        <img src='https://i.pinimg.com/564x/61/79/9d/61799d12359009e53402baf7c064f3b1.jpg'></img>
+        <p>(pochita por si no lo pillaste LOLXDMAFIA) pero espero te guste uwu</p>
+        <br/>
+        <br/>
+        <h1>Ahora sí mi vida, feliz mes. Te dedico esta cartita</h1>
+        <h4>
+        {"Todo empieza con una partida del lol, como no... Pero como todo evento canónico, fue de repente, pero qué evento.\nConocí a Cris con tetas, una chica shitposter, divertida, pero también un encanto, y una frikarda. Y hermosa, mucho. Quiero que sepas\nque me has dado una vuelta 180º a mi vida apareciendo, ahora sólo ansío volver a casa para estar contigo. Sé que estamos lejos\nel uno del otro, pero adoro pasar todo el tiempo contigo, te siento tan cerca a veces que me quemas. Quiero y de todo corazón que seas la última para toda la vida.\nTe amo con locura Isabel, quiero algún día pillemos esa cabaña y plantemos ese huertito que querías plantar. Te amo putita <3"}
+        </h4>
+        <h2>Tu pero anime</h2>
+        <img src='https://cdn.leonardo.ai/users/c1d36a14-4421-4cc8-81cf-36ce96950584/generations/32ca7349-c5d1-499e-bc62-dec68fb40954/Default_Beauty_girl_anime_with_glasses_2.jpg'></img>
+        <h2>pero buenas patas</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
